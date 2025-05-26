080f7f36e7b80bc7c1f847947cf806fc5a401cd8
anhkhoa0610 <23211TT0029@mail.tdc.edu.vn>
2025-05-26 12:48:16 +0700

    code v1

---
+++ database/migrations/2025_05_26_052215_create_category_book_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryBookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('category_book', function (Blueprint $table) {
+            $table->id();
+            $table->integer('category_id');
+            $table->integer('book_id');
+            $table->timestamps();
+            $table->primary(['category_id', 'book_id']);            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_book');
+    }
+}
