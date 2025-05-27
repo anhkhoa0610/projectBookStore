de16ae3556e7964d740145aa633ac0589925568f
anhkhoa0610 <23211TT0029@mail.tdc.edu.vn>
2025-05-27 10:07:44 +0700

    push code

---
+++ app/Models/CategoryBook.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CategoryBook extends Model
+{
+    protected $fillable = [
+        'book_id',
+        'category_id',
+    ];
+
+    public function book()
+    {
+        return $this->belongsTo(Books::class);
+    }
+
+    // Each CategoryBook belongs to a Category
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
---
+++ database/seeders/CategoryBookSeeder.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoryBookSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('category_book')->insert([
+            'category_id' => '1',
+            'book_id' => '1',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '2',
+            'book_id' => '1',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '1',
+            'book_id' => '2',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '7',
+            'book_id' => '2',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '7',
+            'book_id' => '3',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '3',
+            'book_id' => '3',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '7',
+            'book_id' => '4',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '4',
+            'book_id' => '4',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '5',
+            'book_id' => '5',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '6',
+            'book_id' => '5',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '3',
+            'book_id' => '6',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '7',
+            'book_id' => '7',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '8',
+            'book_id' => '7',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '7',
+            'book_id' => '8',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '4',
+            'book_id' => '8',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '9',
+            'book_id' => '9',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '10',
+            'book_id' => '9',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '12',
+            'book_id' => '10',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '13',
+            'book_id' => '11',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '14',
+            'book_id' => '12',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '4',
+            'book_id' => '12',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '12',
+            'book_id' => '13',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '13',
+            'book_id' => '13',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '16',
+            'book_id' => '14',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '13',
+            'book_id' => '14',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '11',
+            'book_id' => '15',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '17',
+            'book_id' => '15',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '11',
+            'book_id' => '16',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '16',
+            'book_id' => '16',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '3',
+            'book_id' => '17',
+        ]);
+
+        DB::table('category_book')->insert([
+            'category_id' => '7',
+            'book_id' => '17',
+        ]);
+
+    }
+}
